a50e1f1d0ef44b2df8be5c74257a08e1653a36a8
yrlp98 <yousef.r1999@gmail.com>
2021-10-24 17:31:10 +0330

    Implement header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h3>Hi Mom!</h3>
+  <AppHeader></AppHeader>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
   name: "App",
+  components: {
+    AppHeader,
+  },
 };
 </script>
