0bdb0c3a76b3ce623b56dd06effb19afabef9250
yrlp98 <yousef.r1999@gmail.com>
2021-10-28 18:39:44 +0330

    Implement fetchImage

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>Image list</div>
+  <div>
+    <img v-for="(image, index) in allImages" :src="image.link" :key="index" />
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "ImageList",
+  computed: mapGetters(["allImages"]),
+  methods: mapActions(["fetchImages"]),
+  created() {
+    this.fetchImages();
+  },
 };
 </script>
