3e199ca14cf54d8a477b38596415b853fa7bcb3a
yrlp98 <yousef.r1999@gmail.com>
2021-10-30 18:00:05 +0330

    Hide images on logout

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="image-container">
-    <img v-for="(image, index) in allImages" :src="image.link" :key="index" />
+  <div>
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="(image, index) in allImages" :src="image.link" :key="index" />
+    </div>
+    <h2 v-else>Log in to get Started!</h2>
   </div>
 </template>
 
@@ -9,7 +12,7 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
-  computed: mapGetters(["allImages"]),
+  computed: mapGetters(["allImages", "isLoggedIn"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
